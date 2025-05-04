9e4e65dd6f11766548d17f882338d64f0c766564
HeartBlin <heartblin@gmail.com>
2025-05-05 00:31:49 +0300

    revamp(lib): Redid helper functions

--- parts/lib/mkHyprMonitors.nix
+++ parts/lib/mkHyprMonitors.nix
@@ -1,7 +1,11 @@
-monitors: let
-  mkString = monitor: let
-    resolution = "${monitor.resolution}@${toString monitor.refreshRate}";
-    position = "${toString monitor.position.x}x${toString monitor.position.y}";
-    scale = toString monitor.scale;
-  in "monitor = ${monitor.name},${resolution},${position},${scale}";
-in builtins.concatStringsSep "\n" (map mkString monitors)
+monitors: builtins.concatStringsSep "\n"
+  (map (monitor: let
+    res = "${monitor.resolution}@${toString monitor.refreshRate}";
+    pos = "${toString monitor.position.x}x${toString monitor.position.y}";
+    scale = let
+      whole = builtins.floor (monitor.scale * 10);
+      intPart = builtins.floor (whole / 10);
+      decPart = whole - (intPart * 10);
+    in "${toString intPart}.${toString decPart}";
+  in "monitor = ${monitor.name}, ${res}, ${pos}, ${scale}"
+) monitors)
--- parts/lib/mkHyprWallpapers.nix
+++ parts/lib/mkHyprWallpapers.nix
@@ -1,15 +1,7 @@
 wallpapers: let
-  inherit (builtins) attrNames concatStringsSep listToAttrs;
-
-  uniqueWallpaper = attrNames (listToAttrs (map
-    (wp: {
-      name = toString wp.wallpaper;
-      value = null;
-    }) wallpapers
-  ));
-
-  preloadString = map (path: "preload = ${path}") uniqueWallpaper;
-  wallpaperString = map (wp: "wallpaper = ${wp.monitor}, ${toString wp.wallpaper}") wallpapers;
-
-  all = concatStringsSep "\n" (preloadString ++ wallpaperString);
-in "${all}"
+  inherit (builtins) concatStringsSep;
+  string = concatStringsSep "\n" (map
+    (wp: "preload = ${toString wp.wallpaper}\nwallpaper = ${wp.monitor}, ${toString wp.wallpaper}" + "\n")
+    wallpapers
+  );
+in string
