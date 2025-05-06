9b5e9cdf4fb47065712f27f612a1a2674d87c1f3
HeartBlin <heartblin@gmail.com>
2025-05-06 16:38:40 +0300

    feat(vscode): Added extensions/options

--- modules/packages/vscode.nix
+++ modules/packages/vscode.nix
@@ -4,69 +4,96 @@ let
   inherit (lib) mkEnableOption mkIf;
   inherit (builtins) toJSON;
   inherit (config.dawn) vscode;
-  inherit (dawn) flakePath hostName userName;
+  inherit (dawn) userName;
 
   vscodeExtended = pkgs.vscode-with-extensions.override {
     vscodeExtensions = with pkgs.vscode-extensions; [
-      aaron-bond.better-comments
-      jnoortheen.nix-ide
-      pkief.material-icon-theme
-      github.copilot
-      github.copilot-chat
+      # Language support
+      jnoortheen.nix-ide # Nix language support
+
+      # GIT tools
+      github.copilot                    # GitHub Copilot
+      github.copilot-chat               # GitHub Copilot Chat
+      github.codespaces                 # GitHub Codespaces
+      github.vscode-github-actions      # GitHub Actions
+      github.vscode-pull-request-github # GitHub Pull Requests
+
+      # UI/UX
+      pkief.material-icon-theme # Icon theme
+      usernamehw.errorlens      # Error highlighting
     ];
   };
 
-  settings = toJSON {
-    # Editor
-    "editor.guides.bracketPairs" = "active";
+  # Settings for VSCode
+  editor = {
     "editor.bracketPairColorization.enabled" = true;
-    "editor.minimap.enabled" = false;
+    "editor.bracketPairColorization.independentColorPoolPerBracketType" = true;
     "editor.cursorBlinking" = "smooth";
     "editor.cursorSmoothCaretAnimation" = "on";
-    "editor.smoothScrolling" = true;
-    "editor.lineHeight" = 22;
+    "editor.fontFamily" = "'Cascadia Code', 'monospace'";
+    "editor.fontLigatures" = true;
     "editor.letterSpacing" = 0.5;
+    "editor.lineHeight" = 22;
+    "editor.minimap.enabled" = false;
     "editor.renderWhitespace" = "all";
+    "editor.scrollball.horizontal" = "hidden";
+    "editor.smoothScrolling" = true;
+    "editor.stickyScroll.enabled" = true;
     "editor.trimAutoWhitespace" = true;
-    "editor.fontFamily" = "Cascadia Code";
+    "editor.wordWrap" = "on";
+  };
 
-    # Explorer
+  explorer = {
     "explorer.compactFolders" = false;
     "explorer.confirmDelete" = false;
     "explorer.confirmDragAndDrop" = false;
+  };
 
-    # Nix
-    "nix.enableLanguageServer" = true;
-    "nix.serverPath" = "nixd";
-    "nix.serverSettings"."nixd" = {
-      "formatting"."command" = [ "nixfmt" ];
-      "options"."nixos"."expr" = "(builtins.getFlake \"${flakePath}\").nixosConfigurations.${hostName}.options";
-    };
+  files = {
+    "files.autoSave" = "onWindowChange";
+    "files.insertFinalNewline" = true;
+    "files.trimTrailingWhitespace" = true;
+  };
 
-    # Workbench
-    "workbench.iconTheme" = "material-icon-theme";
-    "workbench.layoutControl.enabled" = false;
-    "workbench.colorTheme" = "Default Dark+";
-    "workbench.sideBar.location" = "left";
-    "workbench.startupEditor" = "none";
-
-    # Telemetry
+  general = {
+    "extensions.autoCheckUpdates" = false;
+    "extensions.autoUpdate" = false;
+    "redhat.telemetry.enabled" = false;
     "telemetry.telemetryLevel" = "off";
+    "update.mode" = "none";
+    "update.showReleaseNotes" = false;
+  };
 
-    # Terminal
-    "terminal.integrated.fontFamily" = "monospace";
+  nix = {
+    "nix.enableLanguageServer" = true;
+    "nix.serverPath" = "nil";
+  };
 
-    # Window
-    "window.experimentalControlOverlay" = false;
+  terminal = {
+    "terminal.integrated.fontFamily" = "'monospace'";
+    "terminal.integrated.gpuAcceleration" = "on";
+  };
+
+  window = {
     "window.dialogStyle" = "custom";
     "window.menuBarVisibility" = "toggle";
     "window.titleBarStyle" = "custom";
   };
+
+  workbench = {
+    "workbench.colorTheme" = "Default Dark+";
+    "workbench.editor.empty.hint" = "hidden";
+    "workbench.iconTheme" = "material-icon-theme";
+    "workbench.layoutControl.enabled" = false;
+    "workbench.startupEditor" = "none";
+  };
+
+  settings = toJSON (editor // explorer // files // general // nix // terminal // window // workbench);
 in {
   options.dawn.vscode.enable = mkEnableOption "Enables VSCode";
-  
+
   config = mkIf vscode.enable {
-    users.users.${userName}.packages = with pkgs; [ nixd nixfmt-classic statix vscodeExtended ];
+    users.users.${userName}.packages = with pkgs; [ nil nixfmt-classic statix vscodeExtended ];
     homix.".config/Code/User/settings.json".text = settings;
   };
 }
